469c3c4aef404a5256beabc0dff33b6465dffa27
Sergei Filippo <933117@gmail.com>
2026-01-29 11:25:51 +0000

    fix(ingress): remove rewrite-target, add global prefix to backend

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,9 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix('api'); 
   
   app.enableCors(); 
   
-  await app.listen(3000);
+  await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
